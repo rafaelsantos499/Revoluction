3eea7e0ececcf2c9c3dc553f73abddb13eb1a89c
rafaelsantos499 <rafaelsantos499@gmail.com>
2021-07-19 20:41:27 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(){
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute("aria-expanded", active);
+    if(active) {
+        
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir  Menu')
+    }
+
+
+}
+
+btnMobile.addEventListener('click',toggleMenu);
+btnMobile.addEventListener('touchstart',toggleMenu);
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+     delay: 8000,
+    disableOnInteraction: false,
+   },
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
